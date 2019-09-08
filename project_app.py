338e29c3f974721bcab1569af5f6700e9c7f653c
root <root@ip-172-31-5-161.ec2.internal>
2019-09-08 19:19:13 -0400

    updated content of app and tests

--- project_app.py
+++ project_app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 @app.route('/hello/')
 
 def hello_world():
-    return 'Hello World! This is Project v0.2 Dockerizing Jenkins Pipeline\n'
+    return 'Hello World! This is Project v0.3 Dockerizing Jenkins Pipeline\n'
 
 
 @app.route('/hello/<username>') # dynamic route
 
 def hello_user(username):
-    return  '%s Is Dockerizing v0.2 Jenkins Pipeline\n' %username
+    return  '%s Is Dockerizing v0.3 Jenkins Pipeline\n' %username
 
 if __name__ == '__main__':
 
